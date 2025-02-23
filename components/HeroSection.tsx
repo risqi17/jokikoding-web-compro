19dfcbadd33732a944639582bfa3b2063bd29414
Risqi Ahmad Sultoni <ahmadreys@gmail.com>
2025-02-23 19:10:00 +0700

    change tagline

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,19 +6,17 @@ const HeroSection: FC = () => {
     <section className="w-full mx-auto gap-8 flex flex-col py-20 pb-40 ">
       <div className="text-center px-6 py-20 z-40">
         <h1 className="text-4xl md:text-5xl font-bold text-white mb-4">
-          Kami Jokiin Proyek Kodingan Kamu,
+          Koding Profesional, Hasil Optimal
         </h1>
         <span data-aos="fade-up" className="text-5xl text-white aos-init aos-animate">
-          Mulai dari &nbsp;
+          Hanya dengan &nbsp;
           <strong className="relative inline-block">99K &nbsp;
             <Image alt="underline" src="/image/underline.svg" width="108" height="9" decoding="async" data-nimg="1" className="absolute -bottom-3" loading="lazy" /> 
             </strong> 
             Aja!
         </span>
         <p className="text-gray-300 mt-10 max-w-2xl mx-auto">
-          Kami adalah pilihan utama untuk layanan implementasi proyek IT yang terjangkau, efisien,
-          dan dapat diandalkan. Dengan komitmen kami terhadap kualitas, kami menawarkan solusi
-          tercepat dan termurah dalam sesuai kebutuhan.
+        "Hasil optimal" juga berarti bahwa solusi yang dihasilkan mampu membantu klien mencapai tujuan mereka, baik itu meningkatkan efisiensi operasional, mengembangkan produk inovatif, atau memecahkan masalah teknis yang kompleks.
         </p>
       </div>
     </section>
